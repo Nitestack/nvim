780df2a566ec866e4a1b343d3c1a12dc720a3603
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-07 22:56:13 +0100

    update: add mini plugins, update lazy loading events

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -6,4 +6,41 @@ return {
       symbol = core.icons.ui.LineLeft,
     },
   },
+  {
+    "echasnovski/mini.move",
+    keys = core.lazy_map({
+      [{ "n", "x" }] = {
+        [{ "<M-j>", "<M-k>", "<M-h>", "<M-l>" }] = {},
+      },
+    }),
+    opts = {},
+  },
+  {
+    "echasnovski/mini.ai",
+    event = function()
+      return {
+        "LazyFile",
+      }
+    end,
+  },
+  {
+    "echasnovski/mini.comment",
+    event = function()
+      return {}
+    end,
+    keys = core.lazy_map({
+      n = {
+        ["gcc"] = {},
+      },
+      [{ "n", "x", "o" }] = {
+        ["gc"] = {},
+      },
+    }),
+  },
+  {
+    "echasnovski/mini.pairs",
+    event = function()
+      return { "InsertEnter" }
+    end,
+  },
 }
