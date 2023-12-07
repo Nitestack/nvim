00e5552909492ec01a81f0a0c2325af4c832b5a4
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-07 19:27:33 +0100

    feat: added mapping to delete to void register

--- lua/core/mappings.lua
+++ lua/core/mappings.lua
@@ -1,9 +1,19 @@
 local M = {}
 
 ---@type Mappings
-M.mappings = {}
+M.mappings = {
+  [{ "n", "v" }] = {
+    ["<leader>d"] = {
+      [["_d]],
+      "Delete into void register",
+    },
+  },
+}
 
 ---@type KeymapOpts
-M.mapping_opts = {}
+M.mapping_opts = {
+  silent = true,
+  noremap = true,
+}
 
 return M
