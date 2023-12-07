c860aa9fac11d5896d002aa997e7853e844697cd
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-07 19:35:34 +0100

    feat: add plugin related mappings for Telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,6 +10,35 @@ return {
         "Grep (root dir)",
       },
       ["<leader>/"] = { false },
+      ["<leader>fp"] = {
+        function()
+          LazyVimUtil.telescope("find_files", {
+            cwd = require("lazy.core.config").options.root,
+          })
+        end,
+        "Find Plugin File",
+      },
+      ["<leader>fl"] = {
+        function()
+          local files = {} ---@type table<string, string>
+          for _, plugin in pairs(require("lazy.core.config").plugins) do
+            repeat
+              if plugin._.module then
+                local info = vim.loader.find(plugin._.module)[1]
+                if info then
+                  files[info.modpath] = info.modpath
+                end
+              end
+              plugin = plugin._.super
+            until not plugin
+          end
+          LazyVimUtil.telescope("live_grep", {
+            default_text = "/",
+            search_dirs = vim.tbl_values(files),
+          })
+        end,
+        "Find Lazy Plugin Spec",
+      },
     },
   }),
   opts = function(_, opts)
