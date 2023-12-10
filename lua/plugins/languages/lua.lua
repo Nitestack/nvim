97be883910d2c38fe66e9c8d10311f985df697c7
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-10 20:21:09 +0100

    feat: configure Lua lsp and selene

---
+++ lua/plugins/languages/lua.lua
@@ -0,0 +1,37 @@
+---@diagnostic disable: missing-fields
+return utils.lsp.load_language({
+  lsp = {
+    servers = {
+      ---@type lspconfig.options.lua_ls
+      lua_ls = {
+        settings = {
+          Lua = {
+            hint = {
+              enable = true,
+              paramName = "All",
+              paramType = true,
+              arrayIndex = "Disable",
+              setType = true,
+            },
+          },
+        },
+      },
+    },
+  },
+  mason = {
+    "lua-language-server",
+    "stylua",
+    "selene",
+  },
+  formatter = {
+    formatters_by_ft = {
+      ["lua"] = { "stylua" },
+    },
+  },
+  linter = {
+    linters_by_ft = {
+      ["lua"] = { "selene" },
+    },
+  },
+  plugins = {},
+})
