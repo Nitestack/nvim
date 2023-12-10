760f6882419fca440c5040200ef254876aac7fa7
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-10 21:02:24 +0100

    feat: init typescript lsp config

--- lua/plugins/languages/typescript.lua
+++ lua/plugins/languages/typescript.lua
@@ -1,1 +1,124 @@
-return {}
+return utils.lsp.load_language({
+  lsp = {
+    servers = {
+      typescript = {
+        autostart = false,
+        mason = false,
+      },
+    },
+  },
+  mason = {
+    "eslint_d",
+    "prettierd",
+  },
+  treesitter = {
+    "javascript",
+    "prisma",
+    "tsx",
+  },
+  formatter = {
+    formatters = {
+      prettierd = {
+        env = {
+          PRETTIERD_DEFAULT_CONFIG = utils.general.resolve_path({
+            vim.fn.stdpath("config") --[[@as string]],
+            "tools",
+            "formatters",
+            ".prettierrc.json",
+          }),
+        },
+      },
+    },
+    formatters_by_ft = {
+      ["javascript"] = { "prettierd" },
+      ["javascriptreact"] = { "prettierd" },
+      ["typescript"] = { "prettierd" },
+      ["typescriptreact"] = { "prettierd" },
+      ["css"] = { "prettierd" },
+      ["scss"] = { "prettierd" },
+      ["html"] = { "prettierd" },
+      ["json"] = { "prettierd" },
+      ["jsonc"] = { "prettierd" },
+      ["yaml"] = { "prettierd" },
+      ["markdown"] = { "prettierd" },
+      ["markdown.mdx"] = { "prettierd" },
+    },
+  },
+  linter = {
+    linters_by_ft = {
+      ["javascript"] = { "eslint_d" },
+      ["javascriptreact"] = { "eslint_d" },
+      ["typescript"] = { "eslint_d" },
+      ["typescriptreact"] = { "eslint_d" },
+    },
+  },
+  plugins = {
+    {
+      "pmizio/typescript-tools.nvim",
+      dependencies = {
+        "nvim-lua/plenary.nvim",
+        "neovim/nvim-lspconfig",
+      },
+      opts = {
+        settings = {
+          tsserver_local = "en",
+          complete_function_calls = true,
+          include_completions_with_insert_text = true,
+          tsserver_max_memory = 3072,
+          tsserver_format_options = {
+            insertSpaceAfterCommaDelimiter = true,
+            insertSpaceAfterSemicolonInForStatements = true,
+            insertSpaceBeforeAndAfterBinaryOperators = true,
+            insertSpaceAfterConstructor = false,
+            insertSpaceAfterKeywordsInControlFlowStatements = true,
+            insertSpaceAfterFunctionKeywordForAnonymousFunctions = true,
+            insertSpaceAfterOpeningAndBeforeClosingEmptyBraces = true,
+            insertSpaceAfterOpeningAndBeforeClosingNonemptyParenthesis = false,
+            insertSpaceAfterOpeningAndBeforeClosingNonemptyBrackets = false,
+            insertSpaceAfterOpeningAndBeforeClosingNonemptyBraces = true,
+            insertSpaceAfterOpeningAndBeforeClosingTemplateStringBraces = false,
+            insertSpaceAfterOpeningAndBeforeClosingJsxExpressionBraces = false,
+            insertSpaceAfterTypeAssertion = false,
+            insertSpaceBeforeFunctionParenthesis = false,
+            placeOpenBraceOnNewLineForFunctions = false,
+            placeOpenBraceOnNewLineForControlBlocks = false,
+            insertSpaceBeforeTypeAnnotation = true,
+            semicolons = "ignore",
+            indentSwitchCase = true,
+
+            indentSize = vim.o.shiftwidth,
+            convertTabsToSpaces = vim.o.expandtab,
+            tabSize = vim.o.tabstop,
+          },
+          tsserver_file_preferences = {
+            quotePreference = "auto",
+            importModuleSpecifierEnding = "auto",
+            jsxAttributeCompletionStyle = "auto",
+            allowTextChangesInNewFiles = true,
+            providePrefixAndSuffixTextForRename = true,
+            allowRenameOfImportPath = true,
+            includeAutomaticOptionalChainCompletions = true,
+            provideRefactorNotApplicableReason = true,
+            generateReturnInDocTemplate = true,
+            includeCompletionsForImportStatements = true,
+            includeCompletionsWithSnippetText = true,
+            includeCompletionsWithClassMemberSnippets = true,
+            includeCompletionsWithObjectLiteralMethodSnippets = true,
+            useLabelDetailsInCompletionEntries = true,
+            allowIncompleteCompletions = true,
+            displayPartsForJSDoc = true,
+            disableLineTextInReferences = true,
+            includeInlayParameterNameHints = "none",
+            includeInlayParameterNameHintsWhenArgumentMatchesName = false,
+            includeInlayFunctionParameterTypeHints = false,
+            includeInlayVariableTypeHints = false,
+            includeInlayVariableTypeHintsWhenTypeMatchesName = false,
+            includeInlayPropertyDeclarationTypeHints = false,
+            includeInlayFunctionLikeReturnTypeHints = false,
+            includeInlayEnumMemberValueHints = false,
+          },
+        },
+      },
+    },
+  },
+})
