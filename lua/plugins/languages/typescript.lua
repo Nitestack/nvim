09cbf28722875f3eff616c131c048200bdc51de9
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-16 18:55:50 +0100

    feat: add package.json info plugin

--- lua/plugins/languages/typescript.lua
+++ lua/plugins/languages/typescript.lua
@@ -58,6 +58,10 @@ return utils.lsp.load_language({
     { import = "lazyvim.plugins.extras.lang.tailwind" },
     {
       "pmizio/typescript-tools.nvim",
+      event = {
+        "BufReadPre *.ts,*.tsx,*.js,*.jsx",
+        "BufNewFile *.ts,*.tsx,*.js,*.jsx",
+      },
       dependencies = {
         "nvim-lua/plenary.nvim",
         "neovim/nvim-lspconfig",
@@ -119,6 +123,86 @@ return utils.lsp.load_language({
             includeInlayPropertyDeclarationTypeHints = false,
             includeInlayFunctionLikeReturnTypeHints = false,
             includeInlayEnumMemberValueHints = false,
+          },
+        },
+      },
+    },
+    {
+      "vuki656/package-info.nvim",
+      dependencies = {
+        "MunifTanjim/nui.nvim",
+      },
+      event = {
+        "BufReadPre package.json",
+        "BufNewFile package.json",
+      },
+      keys = core.lazy_map({
+        n = {
+          ["<leader>ns"] = {
+            function()
+              require("package-info").show({ force = true })
+            end,
+            "Node.js: Show latest package version",
+          },
+          ["<leader>nh"] = {
+            function()
+              require("package-info").hide()
+            end,
+            "Node.js: Hide package versions",
+          },
+          ["<leader>nt"] = {
+            function()
+              require("package-info").toggle()
+            end,
+            "Node.js: Toggle package versions",
+          },
+          ["<leader>nu"] = {
+            function()
+              require("package-info").update()
+            end,
+            "Node.js: Update dependency",
+          },
+          ["<leader>nd"] = {
+            function()
+              require("package-info").delete()
+            end,
+            "Node.js: Delete dependency",
+          },
+          ["<leader>ni"] = {
+            function()
+              require("package-info").install()
+            end,
+            "Node.js: Install dependency",
+          },
+          ["<leader>nv"] = {
+            function()
+              require("package-info").change_version()
+            end,
+            "Node.js: Change dependency version",
+          },
+        },
+      }, {
+        silent = true,
+        noremap = true,
+      }),
+      opts = {
+        package_manager = "pnpm",
+      },
+    },
+    {
+      "nvim-telescope/telescope.nvim",
+      opts = {
+        load_extensions = {
+          ["package_info"] = true,
+        },
+      },
+    },
+    {
+      "folke/which-key.nvim",
+      opts = {
+        defaults = {
+          ["<leader>n"] = {
+            name = "+node.js",
           },
         },
       },
