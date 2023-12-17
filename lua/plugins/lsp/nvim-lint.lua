d3b9e47b2c5c327512dce1e9a65cfbf193f0ddfa
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-17 22:38:05 +0100

    feat: add key bind to manually lint

---
+++ lua/plugins/lsp/nvim-lint.lua
@@ -0,0 +1,14 @@
+---@type LazyPluginSpec
+return {
+  "mfussenegger/nvim-lint",
+  keys = core.lazy_map({
+    n = {
+      ["<leader>cl"] = {
+        function()
+          require("lint").try_lint()
+        end,
+        "Lint",
+      },
+    },
+  }),
+}
