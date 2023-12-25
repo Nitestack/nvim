fa833ddd0e2922e91727432b6fa4e9b928ff47d8
HydraNhani <hydra.qntp1764@gmail.com>
2023-12-25 17:10:26 +0100

    fix: remove css (duplication)

--- lua/core/config.lua
+++ lua/core/config.lua
@@ -73,7 +73,7 @@ M.ui = {
 --------------------------------------------------------------------------------
 M.plugins = {
   mason = { "codespell" },
-  treesitter = { "css" },
+  treesitter = {},
   linting = {
     linters_by_ft = {
       ["*"] = { "codespell" },
